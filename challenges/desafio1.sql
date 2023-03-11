0ed9dcf198f4d29617bc4b664ee0282cf7a97082
NettoJM <nettojm86@gmail.com>
2023-03-11 12:31:22 -0300

    starting to create normalized tables and inserting data into them

--- challenges/desafio1.sql
+++ challenges/desafio1.sql
@@ -1,23 +1,27 @@
 -- Descomente e altere as linhas abaixo:
 
--- DROP DATABASE IF EXISTS SpotifyClone;
--- CREATE DATABASE IF NOT EXISTS SpotifyClone;
--- CREATE TABLE SpotifyClone.tabela1(
---     coluna1 tipo restricoes,
---     coluna2 tipo restricoes,
---     colunaN tipo restricoes,
--- ) engine = InnoDB;
--- CREATE TABLE SpotifyClone.tabela2(
---     coluna1 tipo restricoes,
---     coluna2 tipo restricoes,
---     colunaN tipo restricoes,
--- ) engine = InnoDB;
--- INSERT INTO SpotifyClone.tabela1 (coluna1, coluna2)
--- VALUES
---   ('exemplo de dados 1', 'exemplo de dados A'),
---   ('exemplo de dados 2', 'exemplo de dados B'),
---   ('exemplo de dados 3', 'exemplo de dados C');
--- INSERT INTO SpotifyClone.tabela2 (coluna1, coluna2)
--- VALUES
---   ('exemplo de dados 1', 'exemplo de dados X'),
---   ('exemplo de dados 2', 'exemplo de dados Y');
+DROP DATABASE IF EXISTS SpotifyClone;
+CREATE DATABASE IF NOT EXISTS SpotifyClone;
+CREATE TABLE SpotifyClone.artista (
+  id INT NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+) engine = InnoDB;
+CREATE TABLE SpotifyClone.plano(
+  plano_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  plano VARCHAR(255) NOT NULL,
+  valor_plano DECIMAL(5,2) NOT NULL
+) engine = InnoDB;
+INSERT INTO SpotifyClone.artista (nome) VALUES 
+  ('Beyoncé'),
+  ('Queen'),
+  ('Elis Regina'),
+  ('Baco Exu do Blues'),
+  ('Blind Guardian'),
+  ('Nina Simone');
+INSERT INTO SpotifyClone.plano (plano, valor_plano)
+VALUES 
+  ('gratuito', 0),
+  ('universitário', 5.99),
+  ('pessoal', 6.99),
+  ('familiar', 7.99);
